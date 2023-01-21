429ecd6fb4f817e5a348b9a86e9caffe6785d3b5
Luan Braz <luan_silvacpv@hotmail.com>
2023-01-21 19:04:27 -0300

    updates

--- src/app/dem/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/dem/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { comparaValidator } from 'src/app/validators/compara-validator';
 import { CpfValidator } from 'src/app/validators/cpf-validator';
 import { Usuario } from './models/usuario';
 
@@ -15,28 +16,46 @@ export class CadastroComponent implements OnInit {
   usuario !: Usuario;
   formResult: string = '';
 
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo Nome é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    cpf: [
+      { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
+      { tipo: 'invalido', mensagem: 'CPF Inválido.' },
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+    senhaConfirmacao: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a Senha.' },
+    ],
+  };
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.cadastroForm = this.fb.group({
-      nome: ['', Validators.required],
-      cpf: ['', Validators.required],
-      // cpf: ['', Validators.required, CpfValidator.cpfValido],
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      cpf: ['', [Validators.required, CpfValidator.cpfValido]],
       email: ['', [Validators.required, Validators.email]],
-      senha: ['',],
-      senhaConfirmacao: ['']
+      senha: ['',[Validators.required, Validators.minLength(6)]],
+      senhaConfirmacao: ['', [Validators.required, Validators.minLength(6)]]
+    }, {
+      // check senhas iguais
+      validator: comparaValidator('senha', 'senhaConfirmacao')
     })
   }
-
-  // carregarFormGroup() {
-  //   this.cadastroForm = new FormGroup({
-  //     nome: new FormControl(''),
-  //     cpf: new FormControl(''),
-  //     email: new FormControl(''),
-  //     senha: new FormControl(''),
-  //     senhaConfirmacao: new FormControl(''),
-  //   })
-  // }
 
   adicionarUsuario() {
 
